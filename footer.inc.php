ec54ec799f4ddd302898546ec917c7b2298dbe72
MRSA-ID <sanggisda@gmail.com>
2021-07-07 14:34:03 +0700

    first commit

---
+++ footer.inc.php
@@ -0,0 +1,11 @@
+			</td>
+		</tr>	
+	<table id="footer" class="template_footer" border="1">
+		<tr>
+			<td>
+				<p>Copyright &copy; 2021 </p>
+			</td>
+		</tr>
+	</table>
+</body>
+</html>
---
+++ header.inc.php
@@ -0,0 +1,29 @@
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Profile Muhammad Budianto</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<table id="header" class="template_header" border="1">
+		<tr>
+			<td>
+				<img src="assets/budi.jpg" alt="Foto profile budi" class="profile">
+			</td>
+			<td>
+				Muhammad Budianto_41519210010 <br>
+				<span>TEKNIK INFORMATIKA FAKULTAS ILMU KOMPUTER UNIVERSITAS MERCU BUANA</span>
+			</td>
+		</tr>
+	</table>
+	<table id="content" class="template_content" border="1">
+		<tr>
+			<td class="menu">
+				<ul>
+					<li><a href="">Home</a></li>
+					<li><a href="">Profile</a></li>
+					<li><a href="">Buku Tamu</a></li>
+					<li><a href="">Kontak</a></li>
+				</ul>
+			</td>
